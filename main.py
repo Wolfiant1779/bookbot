6b4906a0f59d7b87f476fd4a20080890df83f138
Wolfiant1779 <cameron.bunch23@protonmail.com>
2024-10-12 18:00:24 -0700

    main.py add a function to count the number of each letter in the text.

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    num_char = get_num_char(text)
+    print(f"{num_char} characters in the document")
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_char(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
